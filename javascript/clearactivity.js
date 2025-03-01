5ca40cbc779f2c6c4c8a7c32de1ff7cf81c69206
Rajdwipbd <rajdwipmajumder271@gmail.com>
2025-03-01 21:48:14 +0600

    utilities and clearactivity added

---
+++ javascript/clearactivity.js
@@ -0,0 +1,12 @@
+clickCompletebtn("completeBtn1");
+clickCompletebtn("completeBtn2");
+clickCompletebtn("completeBtn3");
+clickCompletebtn("completeBtn4");
+clickCompletebtn("completeBtn5");
+clickCompletebtn("completeBtn6");
+
+document.getElementById("clearActivity")
+    .addEventListener("click", function () {
+        document.getElementById("emty-div").innerText = "";
+    });
+
---
+++ javascript/urilities.js
@@ -0,0 +1,51 @@
+function getInnerText(id) {
+    const innerText = document.getElementById(id).innerText;
+    const convertedInnerText = parseInt(innerText);
+    return convertedInnerText;
+}
+// getInnerText("taskAssined-text");
+
+function clickCompletebtn(id) {
+    document.getElementById(id)
+        .addEventListener("click", function (event) {
+            alert("Borad Updated Sucessfully");
+            document.getElementById(id).disabled = true;
+
+            const my_result = getInnerText("taskAssinedText");
+            const substrac = my_result - 1;
+            document.getElementById("taskAssinedText").innerText = "0" + substrac;
+
+            const NavCounter = getInnerText("nav-counter");
+            // console.log(NavCounter);
+            const sum = NavCounter + 1;
+            document.getElementById("nav-counter").innerText = sum;
+
+            // emty div selected 
+            const emty = document.getElementById("emty-div");
+
+            // helped By Google  to get div an element
+            let parentDiv = this.closest(".task");
+            let taskTitle = parentDiv.querySelector(".exact").innerText;
+
+
+            const newP = document.createElement("p");
+            newP.innerText = "You Have Completed The Task " + taskTitle + " ";
+            newP.classList.add("activity_logStyle");
+
+            const spanTag = document.createElement("span");
+            const now = new Date();
+            spanTag.innerText = `at: ${now.toLocaleTimeString()}`;
+
+            emty.appendChild(newP).appendChild(spanTag);
+
+            
+            if (document.getElementById("completeBtn6").disabled) {
+                alert("Congrats!! You Have Completed All the Task");
+            }
+
+        })
+}
+
+
+
+
