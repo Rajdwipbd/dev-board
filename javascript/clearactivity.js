d593058bc6123922184039156ec2f5a3e6100743
Rajdwipbd <rajdwipmajumder271@gmail.com>
2025-03-02 20:45:33 +0600

    Finally added all

--- javascript/clearactivity.js
+++ javascript/clearactivity.js
@@ -10,12 +10,12 @@ document.getElementById("clearActivity")
         document.getElementById("emty-div").innerText = "";
     });
 
-document.getElementById("discover-btn")
-    .addEventListener("click", function () {
+document.getElementById("discover-btn").addEventListener("click", function () {
         window.location.href = "secondPage.html"
     })
 
 // color change 
+
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -27,14 +27,11 @@ function getRandomColor() {
 
 function changeRandomBackgroundColor() {
     const colors = [];
-    // Generate 5 random colors and add them to the array
     for (let i = 0; i < 5; i++) {
         colors.push(getRandomColor());
     }
-
-    // Use for...of loop to iterate over colors and set a random background color
     for (const color of colors) {
         document.body.style.backgroundColor = color;
-        break; // Exit the loop after changing the background color to one of the random colors
+        
     }
 }
